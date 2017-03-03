7047e1e78b7a205b374bdfb225866c9d2c6b2b48
Andrey Sitnik <andrey@sitnik.ru>
2017-03-03 14:41:09 +0300

    Run spelling check only on latest node

--- bin.js
+++ bin.js
@@ -2,4 +2,9 @@
 
 'use strict'
 
-require('yaspeller/lib/cli')
+if (process.env.TRAVIS && parseInt(process.versions.node) < 7) {
+  console.warn(
+    'To speed up Travis CI tests spelling check runs only on Node.js 7')
+} else {
+  require('yaspeller/lib/cli')
+}
