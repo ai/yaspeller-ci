551e5be3940c0300737b5b2350759314e75db4a7
Andrey Sitnik <andrey@sitnik.ru>
2017-03-05 14:45:51 +0300

    Remove JSDoc hack in favour of expludeFiles config option

--- bin.js
+++ bin.js
@@ -6,7 +6,5 @@ if (process.env.TRAVIS && parseInt(process.versions.node) < 7) {
   console.warn(
     'To speed up Travis CI tests spelling check runs only on Node.js 7')
 } else {
-  const BLACKLIST = /\.js\.html$/
-  process.argv = process.argv.filter(i => !BLACKLIST.test(i))
   require('yaspeller/lib/cli')
 }
