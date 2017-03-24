0150d295ae84b05707f1667510531494eb9b195b
Andrey Sitnik <andrey@sitnik.ru>
2017-03-24 12:32:29 +0300

    Use APPVEYOR_JOB_NUMBER

--- bin.js
+++ bin.js
@@ -6,7 +6,7 @@ function isFirstJob () {
   if (process.env.TRAVIS) {
     return process.env.TRAVIS_JOB_NUMBER.split('.')[1] === '1'
   } else if (process.env.APPVEYOR) {
-    return /nodejs_version=7/.test(process.env.APPVEYOR_JOB_NAME)
+    return process.env.APPVEYOR_JOB_NUMBER === '1'
   } else {
     return true
   }
